e702d401584b00f9e4a6299463481ec37144b60b
Anthony LaTorre <alatorre@caltech.edu>
2022-03-21 15:59:41 -0700

    update

--- scripts/B2DstMu_skimCAND_v1.py
+++ scripts/B2DstMu_skimCAND_v1.py
@@ -1187,9 +1187,10 @@ def create_dSet(n, filepath, cat, applyCorrections=False, skipCut=[], trkControl
                 createSubmissionFile(tmpDir, len(inputs))
                 print 'Submitting jobs'
                 cmd = 'condor_submit %s' % join(tmpDir,'jobs.jdl')
-                cmd += ' -batch-name skim_' + n
+                batch_name = 'skim_%s_%s' % (n,catName)
                 if trkControlRegion:
-                    cmd += '_trkControl'
+                    batch_name += '_trkControl'
+                cmd += ' -batch-name %s' % batch_name
                 status, output = commands.getstatusoutput(cmd)
                 if status != 0:
                     print >> sys.stderr, "Error in processing command: '%s'" % cmd
@@ -1203,8 +1204,7 @@ def create_dSet(n, filepath, cat, applyCorrections=False, skipCut=[], trkControl
                     status, output = commands.getstatusoutput('condor_q')
                     found = False
                     for line in output.split('\n'):
-                        aux = 'skim_'+n+('_trkControl' if trkControlRegion else '')
-                        if aux in line:
+                        if batch_name in line:
                             print line
                             time.sleep(10)
                             found = True
